8bffde3c0800e0e2e2001851247320938cc23329
SamFerguson <fergusonsd@g.cofc.edu>
2020-02-23 16:47:12 -0500

    Add bar graph and more

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
 import React, {Component} from 'react';
 import './App.css';
+import Bar from './Components/Bar';
+import {  } from "module";
 
+const getGraphData = (body) =>{
+  //console.log(body);
+  let premium = {
+    "expense": "Premium",
+  }
+  let deductible = {
+    "expense": "Deductible"
+  }
+  let maxOutPocket = {
+    "expense": "Max Out of Pocket"
+  }
+  body.forEach(element => {
+    premium[element.planName] = element.premium;
+    deductible[element.planName] = element.deductible;
+    maxOutPocket[element.planName] = element.maxOutPocket;
+  });
+  return([premium, deductible, maxOutPocket]);
+
+
+}
 
 class App extends Component {
   state = {
-    plans: []
-  }
+    data: [],
+    graphData: [],
+    hasCompleted: false,
+    shit: [1,2,3,4]
+  };
 
   componentDidMount() {
-    console.log("hi");
-    fetch('http://localhost:5000/plans/5e51e1d605519a4e80b19c3f')
+    //console.log("hi");
+    fetch('http://localhost:5000/plans/')
       .then(res => res.json())
       .then((data) =>{
         this.setState({plans: data})
-        console.log(this.state.plans);
-        console.log(data);
+        this.setState({graphData: getGraphData(data)})
+        console.log(this.state.graphData);
+        this.setState({hasCompleted: true})
+        //console.log(this.state);
+        //console.log(data);
       })
       .catch(console.log)
       
   }
+  
+  
   render() {
+    if(!this.state.hasCompleted){ return(null); }
     return (
-      <div className = "container">
-        <p>{this.state.plans.deductible} {this.state.plans.outOfPocket} {this.state.plans.premium} {this.state.plans.premium}
-        </p>
-      
-          <p>You can{this.state.plans.canKeepDoctor ? '':"'t"} keep your doctor!</p>
-        
+      <div>
+        <div style={{height:600}}>
+          <Bar data={this.state.graphData}/>
+        </div>
       </div>
     );
   }
